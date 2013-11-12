264c2062e6312946d4885e382848294868b47aaa
Rob Eroh <rob@eroh.me>
2013-11-12 22:59:46 +1300

    deploy python

--- deploy/recipes/python.rb
+++ deploy/recipes/python.rb
@@ -6,11 +6,11 @@
 include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
-  if deploy[:application_type] != 'other'
+  if deploy[:application_type] != 'python'
     Chef::Log.debug("Skipping deploy::python application #{application} as it is not an python (other) app")
     next
   end
-  
+
   opsworks_deploy_dir do
     user deploy[:user]
     group deploy[:group]
