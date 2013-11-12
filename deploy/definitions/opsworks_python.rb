af48215db652bb9c0e59471cd7e9657bda3553bd
Rob Eroh <rob@eroh.me>
2013-11-13 01:35:25 +1300

    uwsgi config

--- deploy/definitions/opsworks_python.rb
+++ deploy/definitions/opsworks_python.rb
@@ -9,6 +9,8 @@ define :opsworks_python do
     host "127.0.0.1"
     port 8080
     worker_processes deploy[:cpus]
+    buffer_size 50000
+    http "0.0.0.0:80"
     enable_threads true
     ini "#{deploy[:deploy_to]}/current/uwsgi.ini"
   end
