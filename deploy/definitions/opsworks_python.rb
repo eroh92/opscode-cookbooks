745e5ed769943719143b9c5c2673daeac75f2bb9
Rob Eroh <rob@eroh.me>
2013-11-13 00:55:50 +1300

    flexibile uwsgi path

--- deploy/definitions/opsworks_python.rb
+++ deploy/definitions/opsworks_python.rb
@@ -4,10 +4,12 @@ define :opsworks_python do
 
   uwsgi_service application do
     home_path "#{deploy[:deploy_to]}/current"
+    uwsgi_path "#{deploy[:deploy_to]}/current/bin/uwsgi"
     pid_path "/var/run/uwsgi-app.pid"
     host "127.0.0.1"
     port 8080
     worker_processes deploy[:cpus]
+    enable_threads true
     ini "#{deploy[:deploy_to]}/current/uwsgi.ini"
   end
 
